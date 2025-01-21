8d0c3baff2bc59ec738ff379109467dfb874e9b1
Sean Vieira <svieira151@live.co.uk>
2025-01-21 03:40:03 +0000

    Fixed reference bugs in updated ping command

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,14 +1,14 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getInteractionUserGuildName } from "../../utility/interactionHelpers.js";
 
-const defaultPingMsg = `Yes? Oh... it's you, ${getInteractionUserGuildName(interaction)}. That's a shame.`;
+const defaultPingMsg = (interaction) => `Yes? Oh... it's you, ${getInteractionUserGuildName(interaction)}. That's a shame.`;
 
 export const command = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Will reply if all is well.'),
 	async execute(interaction) {
-		await interaction.reply(defaultPingMsg);
-		interaction.editReply(defaultPingMsg + `\n(*Roundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms*)`);
+		const sent = await interaction.reply(defaultPingMsg(interaction));
+		interaction.editReply(defaultPingMsg(interaction) + `\n\n(Roundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms)`);
 	},
 };
