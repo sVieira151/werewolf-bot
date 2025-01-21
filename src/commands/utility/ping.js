b13760ea672d154baeaaefc2bf4598f5c883ac26
Sean Vieira <svieira151@live.co.uk>
2025-01-21 03:36:49 +0000

    Added minor change to ping to actually display the ping

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,11 +1,14 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getInteractionUserGuildName } from "../../utility/interactionHelpers.js";
 
+const defaultPingMsg = `Yes? Oh... it's you, ${getInteractionUserGuildName(interaction)}. That's a shame.`;
+
 export const command = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Will reply if all is well.'),
 	async execute(interaction) {
-		await interaction.reply(`Yes? Oh... it's you, ${getInteractionUserGuildName(interaction)}. That's a shame.`);
+		await interaction.reply(defaultPingMsg);
+		interaction.editReply(defaultPingMsg + `\n(*Roundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms*)`);
 	},
 };
